1be6f48e2521ebe6336d1d04945baa843f272210
KonstantinAth <13-13113@saeinstitute.edu>
2021-11-13 01:00:34 +0200

    Crouch Functionality Added

--- Wicked_Silence/Assets/Scripts/Camera/CameraMovement.cs
+++ Wicked_Silence/Assets/Scripts/Camera/CameraMovement.cs
@@ -6,21 +6,46 @@ public class CameraMovement : MonoBehaviour
 {
     [SerializeField] [Range(0, 500)] float sensitivity;
     [SerializeField] PlayerMovement player;
+    [SerializeField] float originalPosition = 1.0f;
+    [SerializeField] float crouchingYPosition;
+    [SerializeField] float crouchMultiplier;
     Camera mainCamera;
     float RotationY;
     float RotationX;
     // Start is called before the first frame update
-    void Start() { mainCamera = Camera.main; }
+    Vector3 placeholderPosition;
+    Vector3 crouchPosition;
+    void Start() {
+        mainCamera = Camera.main;
+        placeholderPosition = new Vector3(0.0f, originalPosition, 0.0f);
+        transform.localPosition = placeholderPosition;
+    }
     // Update is called once per frame
     void Update()
     {
         Inputs();
+        Crouching();
     }
     void Inputs() {
-        RotationY = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
-        RotationY = Mathf.Clamp(RotationY, -45.0f, 45.0f);
+        RotationY -= Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
         RotationX = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
-        transform.Rotate(-RotationY, 0.0f, 0.0f);
+        RotationY = Mathf.Clamp(RotationY, -70, 70);
+        transform.localRotation = Quaternion.Euler(RotationY, 0.0f, 0.0f);
+        //transform.Rotate(-RotationY, 0.0f, 0.0f, Space.Self);
         player.transform.Rotate(0.0f, RotationX, 0.0f);
+    }
+    void Crouching() {
+        crouchPosition = new Vector3(0.0f, crouchingYPosition, 0.0f); ;
+        Vector3 positionAtTheTime = new Vector3(0.0f, transform.localPosition.y, 0.0f);
+        if (player.isCrouching || player.IsBelowObject()) {
+            Debug.Log("[CROUCHING]...");
+            mainCamera.transform.localPosition = Vector3.Lerp(positionAtTheTime, crouchPosition, crouchMultiplier * Time.deltaTime);
+        }
+        else {
+            if (mainCamera.transform.localPosition != placeholderPosition) {
+                Debug.Log("NOT IN THE RIGHT POSITION");
+                mainCamera.transform.localPosition = Vector3.Lerp(mainCamera.transform.localPosition, placeholderPosition, crouchMultiplier * Time.deltaTime);
+            }
+        }
     }
 }
--- Wicked_Silence/Assets/Scripts/Player/PlayerMovement.cs
+++ Wicked_Silence/Assets/Scripts/Player/PlayerMovement.cs
@@ -3,25 +3,46 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
+    [Header("Player Movement")]
     [SerializeField] [Range(0, 50)] float moveSpeed = 7;
     [SerializeField] [Range(0, 70)] float runSpeed = 15;
     CharacterController playerController;
+    [Header("Character Controller Configs")]
+    [SerializeField] Vector3 crouchControllerCenter;
+    [SerializeField] float crouchControllerHeight;
+    //Raycast info...
+    [Header("Raycast Info")]
+    Ray ray;
+    [SerializeField] float maxDistance = 2.0f;
+    [SerializeField] LayerMask belowObjectLayerMask;
+    //Input floats
     float XInput;
     float ZInput;
+    //Boolean to indicate if player is pressing the LShift
     bool IsRunning;
+    public bool isCrouching;
+    //Player Movement & direction vectors
     Vector3 movement;
     Vector3 direction;
+    //Holding the object's original Scale...
+    float originalControllerHeight;
+    Vector3 originalControllerCenter;
     // Start is called before the first frame update
     void Start() {
+        //Initializing character controller
         playerController = GetComponent<CharacterController>();
+        //Initializing object's original Controller Configs...
+        originalControllerHeight = playerController.height;
+        originalControllerCenter = playerController.center;
     }
-
     // Update is called once per frame
     void Update() {
         Inputs();
         Movement();
+        Crouching();
     }
     void Movement() {
+        //Change move speed if player input's the LSHift Key...
         if (IsRunning) {
             Debug.Log("RUNNING");
             playerController.Move(direction * runSpeed * Time.deltaTime);
@@ -30,6 +51,7 @@ public class PlayerMovement : MonoBehaviour {
             playerController.Move(direction * moveSpeed * Time.deltaTime);
         }
     }
+    //Taking user's inputs...
     void Inputs() {
         IsRunning = Input.GetKey(KeyCode.LeftShift);
         XInput = Input.GetAxisRaw("Horizontal");
@@ -37,5 +59,26 @@ public class PlayerMovement : MonoBehaviour {
 
         movement = new Vector3(XInput, 0.0f, ZInput);
         direction = transform.TransformDirection(movement).normalized;
+    }
+    void Crouching() {
+        isCrouching = Input.GetKey(KeyCode.LeftControl);
+        if(isCrouching || IsBelowObject()) {
+            playerController.height = crouchControllerHeight;
+            playerController.center = crouchControllerCenter;
+        }
+        else {
+            playerController.height = originalControllerHeight;
+            playerController.center = originalControllerCenter;
+        }
+    }
+    public bool IsBelowObject() {
+        Ray ray = new Ray(transform.position, Vector3.up);
+        bool belowObject = Physics.Raycast(ray, maxDistance, belowObjectLayerMask);
+        if(belowObject) {
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 }
